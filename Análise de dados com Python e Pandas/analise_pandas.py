2bb35188e4864eed261a946c60faa84c2a1b5954
GuilhermeAGonzaga <113460066+GuilhermeAGonzaga@users.noreply.github.com>
2022-10-30 17:44:57 -0300

    Add files via upload
    Atividade da disciplina Análise de dados com Python e Pandas

---
+++ Análise de dados com Python e Pandas/analise_pandas.py
@@ -0,0 +1,97 @@
+#Importando as bibliotecas
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+
+#Upload do arquivo
+from google.colab import files
+arq = files.upload()
+
+#Criando nosso DataFrame
+df = pd.read_excel("AdventureWorks.xlsx")
+
+#Qual a Receita total?
+df["Valor Venda"].sum()
+
+#Qual o custo Total?
+df["custo"] = df["Custo Unitário"].mul(df["Quantidade"])
+
+#Qual o custo Total?
+round(df["custo"].sum(), 2)
+
+#Agora que temos a receita e custo e o total, podemos achar o Lucro total
+#Vamos criar uma coluna de Lucro que será Receita - Custo
+df["lucro"]  = df["Valor Venda"] - df["custo"] 
+
+#Total Lucro
+round(df["lucro"].sum(),2)
+
+#Criando uma coluna com total de dias para enviar o produto
+df["Tempo_envio"] = df["Data Envio"] - df["Data Venda"]
+df["Tempo_envio"] = (df["Data Envio"] - df["Data Venda"]).dt.days
+
+#Extraindo apenas os dias
+df["Tempo_envio"] = (df["Data Envio"] - df["Data Venda"]).dt.days
+
+#Média do tempo de envio por Marca
+df.groupby("Marca")["Tempo_envio"].mean()
+
+#Vamos Agrupar por ano e marca
+df.groupby([df["Data Venda"].dt.year, "Marca"])["lucro"].sum()
+pd.options.display.float_format = '{:20,.2f}'.format
+
+#Resetando o index
+lucro_ano = df.groupby([df["Data Venda"].dt.year, "Marca"])["lucro"].sum().reset_index()
+lucro_ano
+
+#Qual o total de produtos vendidos?
+df.groupby("Produto")["Quantidade"].sum().sort_values(ascending=False)
+
+#Gráfico Total de produtos vendidos
+df.groupby("Produto")["Quantidade"].sum().sort_values(ascending=True).plot.barh(title="Total Produtos Vendidos")
+plt.xlabel("Total")
+plt.ylabel("Produto");
+
+#Grafico lucro x Ano
+df.groupby(df["Data Venda"].dt.year)["lucro"].sum().plot.bar(title="Lucro x Ano")
+plt.xlabel("Ano")
+plt.ylabel("Receita");
+
+#Selecionando apenas as vendas de 2009
+df_2009 = df[df["Data Venda"].dt.year == 2009]
+
+#Grafico lucro x Mês 2009
+df_2009.groupby(df_2009["Data Venda"].dt.month)["lucro"].sum().plot(title="Lucro x Mês")
+plt.xlabel("Mês")
+plt.ylabel("Lucro");
+
+#Grafico lucro x Marca 2009
+df_2009.groupby("Marca")["lucro"].sum().plot.bar(title="Lucro x Marca")
+plt.xlabel("Marca")
+plt.ylabel("Lucro")
+plt.xticks(rotation='horizontal');
+
+#Grafico lucro x Classe 2009
+df_2009.groupby("Classe")["lucro"].sum().plot.bar(title="Lucro x Classe")
+plt.xlabel("Classe")
+plt.ylabel("Lucro")
+plt.xticks(rotation='horizontal');
+
+#Gráfico de Boxplot
+df["Tempo_envio"].describe()
+plt.boxplot(df["Tempo_envio"]);
+
+#Histograma
+plt.hist(df["Tempo_envio"]);
+
+#Tempo mínimo de envio
+df["Tempo_envio"].min()
+
+#Tempo máximo de envio
+df['Tempo_envio'].max()
+
+#Identificando o Outlier
+df[df["Tempo_envio"] == 20]
+
+#Salvando Arquivo em csv
+df.to_csv("df_vendas_novo.csv", index=False)
